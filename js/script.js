38686c7c05354228e3bcc2219bae8ae156bb1097
Knievilkangaroo <Knievilkangaroo@gmail.com>
2023-06-06 15:52:49 -0500

    added js, more to come

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let numOfStcks = 0;
+let outPut = 0;
+let input = '';
+let choice = 0; // default choice out of 4
+let times = 0;
+
+
+// if user picks a alloy do this
+
+if (choice = 0) { // no imput
+    console.log("no user choice")
+    return "Please select an alloy."
+} 
+
+if (choice = 1) { // alluminum brass
+    console.log
+    times =
+    return
+}
+
+if (choice = 2) { // alumnite
+    console.log
+    times =
+    return
+}
+
+if (choice = 3) { // bronze
+    console.log
+    times =
+    return
+}
+
+if (choice = 4) { // manyullyn
+    console.log
+    times =
+    return
+}
+
+// Now do the math robot
+
+
+//send it to the user :33
+
